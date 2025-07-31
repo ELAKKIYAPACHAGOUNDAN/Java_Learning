b04c6873e86e25212322330d1e0e556656e29b93
ELAKKIYAPACHAGOUNDAN <elakkiya0915@gmail.com>
2025-07-31 08:22:30 +0530

    modified -2

--- Mutable/String_buffer_builder.java
+++ Mutable/String_buffer_builder.java
@@ -8,15 +8,25 @@ public class String_buffer_builder {
 		//initial capacity
 		StringBuffer str = new StringBuffer();
 		System.out.println(str.capacity());
-		
+		System.out.println("---");
 		//input as append()
-		StringBuffer s1= new StringBuffer("hello world hiiiiiiiiiiii");
-		s1.append("This");
-		s1.append(" is ");
-		s1.append("wow");
+		StringBuffer s1 = new StringBuffer("hello");
+		s1.append("hello");
+		s1.append(" world");
 		System.out.println(s1);
-		System.out.println(s1.capacity());
+		System.out.println("length: " + s1.length());
+		System.out.println("capacity: " + s1.capacity()); 
+		System.out.println("---");
+		
+		//input as insert()
+		StringBuffer s2 = new StringBuffer();
+		s2.append("hello");
+		s2.append("world is full of mystry");
+		System.out.println(s2);
+		System.out.println("Length: " + s2.length());
+		System.out.println("Capacity: " + s2.capacity());
 
+		
 	}
 
 }
